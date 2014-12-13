838f98234b08d98262b603e66b438974cbf741d6
chaceb94 <chaceb94@gmail.com>
2014-12-13 14:39:06 -0800

    t

--- www/js/index.js
+++ www/js/index.js
@@ -75,11 +75,11 @@ function move(){
     y = y + yMove;
     if(x >= court.width - ballSize || x <= 0){
         xMove = -xMove;
-        console.log(navigator.vibrate);
+        console.log(navigator.vibrate(50));
     }
     if(y >= court.height - ballSize || y <= 0){
         yMove = -yMove;
-        console.log(navigator.vibrate);
+        navigator.vibrate(50);
     }
     ctx.clearRect(0,0,court.width,court.height);
     ctx.fillRect(x,y,ballSize,ballSize);
